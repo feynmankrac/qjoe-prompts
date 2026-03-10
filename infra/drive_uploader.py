5b3d37a7a24bcc72a59a85bfec91687a776c95a9
Ely <Henry.Lebesgues@gmail.com>
2026-03-10 20:14:13 +0100

    fix drive oauth for VPS

--- infra/drive_uploader.py
+++ infra/drive_uploader.py
@@ -32,7 +32,8 @@ def get_drive_service():
             flow = InstalledAppFlow.from_client_secrets_file(
                 CREDENTIALS_FILE, SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            #creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
 
         with open(TOKEN_FILE, "wb") as token:
             pickle.dump(creds, token)
